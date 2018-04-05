a68da1b9176bce855b81ca3514eb060137c6ebce
Szymon Nosek <esnosek@gmail.com>
2018-04-05 12:55:06 +0100

    Improve logistic regression model

--- src/data_parser.py
+++ src/data_parser.py
@@ -3,28 +3,46 @@ import cv2
 import numpy as np
 
 data_dir = "data/"
-data_file = data_dir + "/labels.csv"
+data_file = data_dir + "labels.csv"
 width = 200
 height = 200
+channels = 3
 class_number = 2
 
-def get_binary_labeled_data(gray = False):
-    channels = 1 if gray else 3
+
+def get_binary_labeled_data(test, one_hot = False):
+    
     data = pd.read_csv(data_file, sep='\t')
+    data = data.sample(frac = 1).reset_index(drop=True)
     size = data["file_name"].size
-    images = np.zeros((size, width, height, channels), dtype = 'float32')
-    labels = np.zeros((size, class_number), dtype = 'float32')
-    for i in range(data["file_name"].size):
+    images = np.zeros((size, width * height * channels), dtype = 'float32')
+    labels = np.zeros((size), dtype = 'float32') 
+    one_hot_labels = np.zeros((size, class_number), dtype = 'float32')
+    
+    for i in range(size):
         file_name = data_dir + data["file_name"][i]
-        images[i] = cv2.imread(file_name, channels)
-        if data["piece"][i] == "EMPTY":
-            labels[i][0] = 1
-        else:
-            labels[i][1] = 1
+        img = cv2.imread(file_name, channels)
+        images[i] = img.reshape((width * height * channels))
+        class_index = 0 if data["piece"][i] == "EMPTY" else 1
+        one_hot_labels[i][class_index] = 1
+        labels[i] = 0 if data["piece"][i] == "EMPTY" else 1
     
-    return images.reshape((size, width*height*channels)), labels
+    end = int(test * size)
     
+    train_images = images[0:end, :]
+    train_labels = labels[0:end]
+    train_one_hot_labels = one_hot_labels[0:end, :]
+
+    test_images = images[end:, :]
+    test_labels = labels[end:]
+    test_one_hot_labels = one_hot_labels[end:, :]
     
+    if one_hot: 
+        return train_images, train_one_hot_labels, test_images, test_one_hot_labels
+    else:
+        return train_images, train_labels, test_images, test_labels
+    
+
 if __name__ == '__main__':
     data_file = data_dir + "/labels.csv"
-    images, labels = get_binary_labeled_data()
+    train_images, train_labels, test_images, test_labels = get_binary_labeled_data(0.8)
--- src/logistic_regression.py
+++ src/logistic_regression.py
@@ -5,21 +5,47 @@ class_number = 2
 width = 200
 height = 200
 channels = 3
-training_epochs = 1000
+batch_size = 100
+epochs = 1000
 display_step = 1
 
-X = tf.placeholder(tf.float32, [None, width, height, channels])
-Y = tf.placeholder(tf.float32, [None, class_number])
-W = tf.Variable(tf.random_normal([width * height * channels, class_number], stddev=0.01))
-b = tf.Variable(tf.zeros([1, class_number]))
+tf.reset_default_graph()
 
-XX = tf.reshape(X, [-1, width * height * channels])
+# Getting data
+train_images, train_labels, test_images, test_labels = get_binary_labeled_data(0.7, one_hot=True)
 
-logits = tf.matmul(XX, W) + b
+train_size = train_images.shape[0]
+batch_size = train_size if train_size < batch_size else batch_size 
 
-entropy = tf.nn.softmax_cross_entropy_with_logits(logits = logits, labels = Y)
-loss = tf.reduce_mean(entropy) # computes the mean over examples in the batch
+# tf Graph Input
+images = tf.placeholder(tf.float32, [None, width * height * channels])
+labels = tf.placeholder(tf.int32, shape=([None, class_number]))
+
+# Set model weights
+W = tf.Variable(tf.random_normal([width * height * channels, class_number], stddev=0.1))
+b = tf.Variable(tf.zeros([class_number]))
+
+# A simple fully connected with two class and a softmax is equivalent to Logistic Regression.
+#logits = tf.contrib.layers.fully_connected(inputs=images, num_outputs=class_number)
+#entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels)
+#loss_tensor = tf.reduce_mean(entropy)
+
+# Construct model
+logits = tf.matmul(images, W) + b
+entropy = tf.nn.softmax_cross_entropy_with_logits(logits=logits, labels=labels)
+loss = tf.reduce_mean(entropy)
+regularizer = tf.nn.l2_loss(W)
+loss = tf.reduce_mean(loss + 0.001 * regularizer)
+
+# Gradient Descent Optimizer
 optimizer = tf.train.GradientDescentOptimizer(0.01).minimize(loss)
+
+# Evaluate the model
+preds = tf.nn.softmax(logits)
+prediction_correct = tf.equal(tf.argmax(preds, 1), tf.argmax(labels, 1))
+
+# Accuracy calculation
+accuracy = tf.reduce_mean(tf.cast(prediction_correct, tf.float32))
   
 # Initialize the variables (i.e. assign their default value)
 init = tf.global_variables_initializer()
@@ -27,22 +53,35 @@ init = tf.global_variables_initializer()
 # Start training
 with tf.Session() as sess:
 
-    all_x, all_y = get_binary_labeled_data()
-    size = all_x.shape[0]
-
     # Run the initializer
     sess.run(init)
 
     # Training cycle
-    for epoch in range(training_epochs):
-        sess.run([optimizer, loss], feed_dict={XX: all_x, Y:all_y})
+    for epoch in range(epochs):
+        total_batch = int(train_size/batch_size)
+        avg_train_loss = 0;
+        avg_train_accuracy = 0;
+        
+        for batch in range(total_batch):
+            
+            start = batch * batch_size
+            end = start + batch_size
+            
+            batch_images = train_images[start:end, :]
+            batch_labels = train_labels[start:end, :]
+            
+            _, train_loss, train_accuracy = sess.run([optimizer, loss, accuracy], feed_dict={images: batch_images, labels: batch_labels})
+            
+            avg_train_loss += train_loss/total_batch
+            avg_train_accuracy += train_accuracy/total_batch
+            
+        if (epoch + 1) % display_step == 0:
+            test_accuracy = sess.run(accuracy, feed_dict={images: test_images, labels: test_labels})
+            print("Epoch " + str(epoch + 1))
+            print("   Training loss: {:.2f}".format(avg_train_loss))
+            print("   Training accuracy: {:.2f} %".format(100 * avg_train_accuracy))
+            print("   Test accuracy: {:.2f} %".format(100 * test_accuracy))
 
-    print("Optimization Finished!")
-
-    total_correct_preds = 0
-    _, loss_batch, logits_batch = sess.run([optimizer, loss, logits], feed_dict={XX: all_x, Y:all_y})
-    preds = tf.nn.softmax(logits_batch)
-    correct_preds = tf.equal(tf.argmax(preds, 1), tf.argmax(all_y, 1))
-    accuracy = tf.reduce_sum(tf.cast(correct_preds, tf.float32))
-    total_correct_preds += sess.run(accuracy)
-    print("Accuracy {0}".format(total_correct_preds/size))
+    # Evaluation of the model
+    test_accuracy = sess.run(accuracy, feed_dict={images: test_images, labels: test_labels})
+    print("Final test accuracy: {:.2f} %".format(100 * test_accuracy))
