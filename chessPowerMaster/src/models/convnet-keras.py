173f89c33d2cb689c073456f5c683253c42db408
esnosek <esnosek@gmail.com>
2019-05-04 01:54:40 +0200

    add rotation to images and add callbacks to keras convnet model

--- chessPowerMaster/src/models/convnet-keras.py
+++ chessPowerMaster/src/models/convnet-keras.py
@@ -1,6 +1,20 @@
 import tensorflow as tf
 from data_parser import get_binary_labeled_data
 
+class TestCallback(tf.keras.callbacks.Callback):
+    def __init__(self, test_data):
+        self.test_data = test_data
+
+    def on_epoch_end(self, epoch, logs={}):
+        x, y = self.test_data
+        loss, acc = self.model.evaluate(x, y)
+        if logs["acc"] == 1.0 and acc == 1.0:
+            print('\nReached 100% accuracy for test and training data so cancelling training!')
+            self.model.stop_training = True
+        else:
+            print('\nTesting loss: {}, acc: {}\n'.format(loss, acc))
+  
+ 
 class_number = 2
 width = 50
 height = 50
@@ -8,14 +22,19 @@ channels = 3
 
 training_images, training_labels, test_images, test_labels = get_binary_labeled_data(0.8, one_hot=False)
 
+
 train_size = training_images.shape[0]
 training_images=training_images / 255.0
 
 test_size = test_images.shape[0]
 test_images=test_images / 255.0
 
+callbacks = TestCallback((test_images, test_labels))   
+
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Conv2D(64, (3,3), activation='relu', input_shape=(width, height, channels)),
+  tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(width, height, channels)),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Conv2D(32, (3,3), activation='relu', input_shape=(width, height, channels)),
   tf.keras.layers.MaxPooling2D(2, 2),
   tf.keras.layers.Flatten(),
   tf.keras.layers.Dense(128, activation='relu'),
@@ -24,6 +43,6 @@ model = tf.keras.models.Sequential([
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 model.summary()
-model.fit(training_images, training_labels, epochs=20)
+model.fit(training_images, training_labels, epochs=20, callbacks=[callbacks])
 print(model.evaluate(test_images, test_labels))
 print(model.evaluate(training_images, training_labels))
--- chessPowerMaster/src/models/data_parser.py
+++ chessPowerMaster/src/models/data_parser.py
@@ -15,19 +15,36 @@ def get_binary_labeled_data(test, one_hot = False):
     data = pd.read_csv(data_file, sep='\t')
     data = data.sample(frac = 1).reset_index(drop=True)
     size = data["file_name"].size
-    images = np.zeros((size, width, height, channels), dtype = 'float32')
-    labels = np.zeros((size), dtype = 'float32') 
-    one_hot_labels = np.zeros((size, class_number), dtype = 'float32')
+    # 4 * size because of image rotation
+    images = np.zeros((4 * size, width, height, channels), dtype = 'float32')
+    labels = np.zeros((4 * size), dtype = 'float32') 
+    one_hot_labels = np.zeros((4 * size, class_number), dtype = 'float32')
+    
+    #rotation matrixes
+    M90 = cv2.getRotationMatrix2D((width / 2, height / 2), 90, 1.0)
+    M180 = cv2.getRotationMatrix2D((width / 2, height / 2), 180, 1.0)
+    M270 = cv2.getRotationMatrix2D((width / 2, height / 2), 270, 1.0)
     
     for i in range(size):
         file_name = data_dir + data["file_name"][i]
         img = cv2.imread(file_name, channels)
         images[i] = img
+        images[size + i] = cv2.warpAffine(img, M90, (width, height))
+        images[2 * size + i] = cv2.warpAffine(img, M180, (width, height))
+        images[3 * size + i] = cv2.warpAffine(img, M270, (width, height))
         class_index = 0 if data["piece"][i] == "EMPTY" else 1
         one_hot_labels[i][class_index] = 1
-        labels[i] = 0 if data["piece"][i] == "EMPTY" else 1
+        one_hot_labels[size + i][class_index] = 1
+        one_hot_labels[2 * size + i][class_index] = 1
+        one_hot_labels[3 * size + i][class_index] = 1
+        label = 0 if data["piece"][i] == "EMPTY" else 1
+        labels[i] = label
+        labels[size + i] = label
+        labels[2 * size + i] = label
+        labels[3 * size + i] = label
     
-    end = int(test * size)
+    end = int(test * size * 4)
+    
     
     train_images = images[0:end, :]
     train_labels = labels[0:end]
@@ -36,6 +53,8 @@ def get_binary_labeled_data(test, one_hot = False):
     test_images = images[end:, :]
     test_labels = labels[end:]
     test_one_hot_labels = one_hot_labels[end:, :]
+    
+
     
     if one_hot: 
         return train_images, train_one_hot_labels, test_images, test_one_hot_labels
